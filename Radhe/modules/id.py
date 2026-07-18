8f444350e196a7ce8cc8dc1d3780474ebe2fd1ca
alonepro098 <aloneee772@gmail.com>
2026-07-18 15:16:51 +0530

    Upload Radhe/modules/id.py via API

---
+++ Radhe/modules/id.py
@@ -0,0 +1,57 @@
+from pyrogram import filters
+from pyrogram.enums import ParseMode
+
+from Radhe import Radhe
+
+
+@Radhe.on_cmd("id")
+async def getid(client, message):
+    chat = message.chat
+    your_id = message.from_user.id if message.from_user else None
+    message_id = message.id
+    reply = message.reply_to_message
+
+    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
+    if your_id:
+        text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if len(message.command) == 2:
+        try:
+            split = message.text.split(None, 1)[1].strip()
+            user = await client.get_users(split)
+            if user:
+                text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user.id})** `{user.id}`\n"
+        except Exception:
+            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
+
+    if chat.username:
+        text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    else:
+        text += f"**ᴄʜᴀᴛ ɪᴅ:** `{chat.id}`\n\n"
+
+    if (
+        reply
+        and not getattr(reply, "empty", True)
+        and not message.forward_from_chat
+        and not reply.sender_chat
+    ):
+        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
+        if reply.from_user:
+            text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+        else:
+            text += f"**ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:** `None`\n\n"
+
+    if reply and reply.forward_from_chat:
+        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+
+    if reply and reply.sender_chat:
+        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
+
+    await message.reply_text(
+        text,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.DEFAULT,
+    )
